c51f44d2b260efbf1cb2b7aba7d71d47ce271651
julianParrax <julianfranco.parra@gmail.com>
2022-12-20 01:04:15 -0300

    Se agrega navegacion

--- src/routes/index_routes.js
+++ src/routes/index_routes.js
@@ -3,18 +3,16 @@ import {  Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render('index.ejs', { title : "Hello to my portfolio!"});//render acepta como parmetro un archivo de la carpeta viws. Para eso configuramos el dirname
+  res.render('index.ejs', { title : "Hello to my portfolio!"});//render acepta como parmetro un archivo de la carpeta viws. Para eso configuramos el dirname; index.js es el archivo que se renderiza y title  es un valor al que podemos acceder.
 });
 
 router.get("/about", (req, res) => {
-  res.render('about.ejs',{title : "About me", description: "About julian"})
+  res.render('about.ejs',{title : "About me", description: "About julian..."})
 });
 
 router.get("/contact", (req,res ) => {
-  res.render('contact.ejs', {title: "Contact"});
+  res.render('contact.ejs', {title: "Contact me  bro", description: "My social networks"});
 });
-
-
 
 
 export default router;
